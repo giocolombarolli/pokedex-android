bae7d7c57276ec13b82ee67408a33a7cb5c915b1
giova <giocolombarolli@gmail.com>
2023-08-26 21:54:26 -0300

    feat: retrofit caller

--- app/src/main/java/br/com/giocolombarolli/pokedex_android/api/PokemonRepository.kt
+++ app/src/main/java/br/com/giocolombarolli/pokedex_android/api/PokemonRepository.kt
@@ -1,4 +1,11 @@
 package br.com.giocolombarolli.pokedex_android.api
 
-class PokemonRepository {
+object PokemonRepository {
+    // https://pokeapi.co/api/v2/pokemon/?limit=151
+
+    init {
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl("https://pokeapi.co/api/v2/")
+//            .addConverterFactory(GsonConverterFactory.create)
+    }
 }
