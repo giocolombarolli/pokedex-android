2bd5f7e689257b30b381bdaedc767ef1d25864d2
giova <giocolombarolli@gmail.com>
2023-08-29 00:03:29 -0300

    feat: searchBar and filter.

--- app/src/main/java/br/com/giocolombarolli/pokedex_android/view/MainActivity.kt
+++ app/src/main/java/br/com/giocolombarolli/pokedex_android/view/MainActivity.kt
@@ -1,6 +1,9 @@
 package br.com.giocolombarolli.pokedex_android.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,6 +16,8 @@ import br.com.giocolombarolli.pokedex_android.viewmodel.PokemonViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: EditText
+
     private val viewModel by lazy {
         ViewModelProvider(this, PokemonViewModelFactory())[PokemonViewModel::class.java]
     }
@@ -20,15 +25,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        searchView = findViewById(R.id.svPokemons)
         recyclerView = findViewById(R.id.rvPokemons)
 
         viewModel.pokemons.observe(this, Observer {
             loadRecyclerView(it)
         })
 
-    }
+        searchView.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val searchText = s.toString()
+                viewModel.filterPokemonByType(searchText)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+        viewModel.filteredPokemon.observe(this, Observer { filteredList ->
+            loadRecyclerView(filteredList)
+        })
+    }
     private fun loadRecyclerView(pokemons: List<Pokemon?>) {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = PokemonAdapter(pokemons)
--- app/src/main/java/br/com/giocolombarolli/pokedex_android/viewmodel/PokemonViewModel.kt
+++ app/src/main/java/br/com/giocolombarolli/pokedex_android/viewmodel/PokemonViewModel.kt
@@ -7,6 +7,7 @@ import br.com.giocolombarolli.pokedex_android.domain.Pokemon
 
 class PokemonViewModel : ViewModel() {
     var pokemons = MutableLiveData<List<Pokemon?>>()
+    val filteredPokemon = MutableLiveData<List<Pokemon?>>()
 
     init {
         Thread(Runnable {
@@ -35,5 +36,18 @@ class PokemonViewModel : ViewModel() {
                 }
             })
         }
+    }
+
+    fun filterPokemonByType(type: String) {
+        val originalList = pokemons.value ?: emptyList()
+
+        val filteredList = if (type.isNotBlank()) {
+            originalList.filter { pokemon ->
+                pokemon!!.types.any { it.name.equals(type, ignoreCase = true) }
+            }
+        } else {
+            originalList
+        }
+        filteredPokemon.value = filteredList
     }
 }
